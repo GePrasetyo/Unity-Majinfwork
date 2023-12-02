37c3743df30afa1cbe92fa6ae36bf1000cfa9142
GePrasetyo <dhimasgppantoja@gmail.com>
2023-12-03 00:50:51 +0100

    Added default loading

---
+++ Runtime/Scripts/Framework/Utilities/DefaultLoadingStreamer.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace Majingari.Framework.World {
+    public class DefaultLoadingStreamer : LoadingStreamer {
+        public override void Construct() {
+
+        }
+    }
+}
