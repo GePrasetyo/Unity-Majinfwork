3ef8f7e29a0e92cf537aa94764c0852db688a154
GePrasetyo <dhimasgppantoja@gmail.com>
2023-12-09 21:18:45 +0100

    Sanity check

--- Runtime/Scripts/Framework/Managers/PlayerController.cs
+++ Runtime/Scripts/Framework/Managers/PlayerController.cs
@@ -3,12 +3,12 @@ using UnityEngine;
 namespace Majingari.Framework.World {
     public abstract class PlayerController : MonoBehaviour {
         protected static bool GetPawn<P>(out P pawn) where P : PlayerPawn {
-            pawn = GameModeManager.playerReference.GetPawn() as P;
+            pawn = GameModeManager.playerReference?.GetPawn() as P;
             return pawn != null;
         }
 
         protected static bool GetState<S>(out S state) where S : PlayerState {
-            state = GameModeManager.playerReference.GetState() as S;
+            state = GameModeManager.playerReference?.GetState() as S;
             return state != null;
         }
     }
