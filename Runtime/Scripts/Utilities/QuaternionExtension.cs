7965d71734aa50bfa151d6fdf5c869eb5a7a848d
GePrasetyo <dhimasgppantoja@gmail.com>
2023-12-10 17:58:41 +0100

    Quaternion and Vector Exrtension

---
+++ Runtime/Scripts/Utilities/QuaternionExtension.cs
@@ -0,0 +1,91 @@
+using UnityEngine;
+
+namespace Majingari.Framework {
+    public static class QuaternionExtension {
+        public static Quaternion IsolateXRotation(this Quaternion q) {
+            float theta_x = Mathf.Atan2(q.x, q.w);
+
+            Quaternion xRotation = new Quaternion(Mathf.Sin(theta_x), 0, 0, Mathf.Cos(theta_x));
+
+            return xRotation;
+        }
+
+        public static Quaternion IsolateYRotation(this Quaternion q) {
+            float theta_y = Mathf.Atan2(q.y, q.w);
+
+            Quaternion yRotation = new Quaternion(0, Mathf.Sin(theta_y), 0, Mathf.Cos(theta_y));
+
+            return yRotation;
+        }
+
+        public static Quaternion IsolateZRotation(this Quaternion q) {
+            float theta_z = Mathf.Atan2(q.z, q.w);
+
+            Quaternion zRotation = new Quaternion(0, 0, Mathf.Sin(theta_z), Mathf.Cos(theta_z));
+
+            return zRotation;
+        }
+
+        public static Quaternion ClampRotationX(this Quaternion q, float angleInDegrees) {
+            Quaternion output = q;
+
+            output.x /= output.w;
+            output.w = 1.0f;
+
+            float angleX = 2.0f * Mathf.Rad2Deg * Mathf.Atan(output.x);
+            angleX = Mathf.Clamp(angleX, -angleInDegrees, angleInDegrees);
+            output.x = Mathf.Tan(0.5f * Mathf.Deg2Rad * angleX);
+
+            return output;
+        }
+
+        public static Quaternion ClampRotationY(this Quaternion q, float angleInDegrees) {
+            Quaternion output = q;
+
+            output.y /= output.w;
+            output.w = 1.0f;
+
+            float angleY = 2.0f * Mathf.Rad2Deg * Mathf.Atan(output.y);
+            angleY = Mathf.Clamp(angleY, -angleInDegrees, angleInDegrees);
+            output.y = Mathf.Tan(0.5f * Mathf.Deg2Rad * angleY);
+
+            return output;
+        }
+
+        public static Quaternion ClampRotationZ(this Quaternion q, float angleInDegrees) {
+            Quaternion output = q;
+
+            output.z /= output.w;
+            output.w = 1.0f;
+
+            float angleZ = 2.0f * Mathf.Rad2Deg * Mathf.Atan(output.z);
+            angleZ = Mathf.Clamp(angleZ, -angleInDegrees, angleInDegrees);
+            output.z = Mathf.Tan(0.5f * Mathf.Deg2Rad * angleZ);
+
+            return output;
+        }
+
+        public static Quaternion ClampRotationXYZ(this Quaternion q, Vector3 bounds) {
+            Quaternion output = q;
+
+            output.x /= output.w;
+            output.y /= output.w;
+            output.z /= output.w;
+            output.w = 1.0f;
+
+            float angleX = 2.0f * Mathf.Rad2Deg * Mathf.Atan(output.x);
+            angleX = Mathf.Clamp(angleX, -bounds.x, bounds.x);
+            output.x = Mathf.Tan(0.5f * Mathf.Deg2Rad * angleX);
+
+            float angleY = 2.0f * Mathf.Rad2Deg * Mathf.Atan(output.y);
+            angleY = Mathf.Clamp(angleY, -bounds.y, bounds.y);
+            output.y = Mathf.Tan(0.5f * Mathf.Deg2Rad * angleY);
+
+            float angleZ = 2.0f * Mathf.Rad2Deg * Mathf.Atan(output.z);
+            angleZ = Mathf.Clamp(angleZ, -bounds.z, bounds.z);
+            output.z = Mathf.Tan(0.5f * Mathf.Deg2Rad * angleZ);
+
+            return output;
+        }
+    }
+}
---
+++ Runtime/Scripts/Utilities/VectorExtension.cs
@@ -0,0 +1,134 @@
+using UnityEngine;
+
+namespace Majingari.Framework {
+    public static class VectorExtension {
+        private enum Axis { X, Y, Z, XY, XZ, YZ }
+        public static Vector3 ExcludingX(this Vector3 self) => Excluding(self, Axis.X);
+        public static Vector3 ExcludingY(this Vector3 self) => Excluding(self, Axis.Y);
+        public static Vector3 ExcludingZ(this Vector3 self) => Excluding(self, Axis.Z);
+        public static Vector3 ExcludingXY(this Vector3 self) => Excluding(self, Axis.XY);
+        public static Vector3 ExcludingXZ(this Vector3 self) => Excluding(self, Axis.XZ);
+        public static Vector3 ExcludingYZ(this Vector3 self) => Excluding(self, Axis.YZ);
+
+        private static Vector3 Excluding(this Vector3 self, Axis axis) {
+            switch (axis) {
+                case Axis.X:
+                    return new Vector3(0, self.y, self.z);
+                case Axis.Y:
+                    return new Vector3(self.x, 0, self.z);
+                case Axis.Z:
+                    return new Vector3(self.x, self.y, 0);
+                case Axis.XY:
+                    return new Vector3(0, 0, self.z);
+                case Axis.XZ:
+                    return new Vector3(0, self.y, 0);
+                case Axis.YZ:
+                    return new Vector3(self.x, 0, 0);
+                default:
+                    return self;
+            }
+
+        }
+
+        public static Vector3 InvertX(this Vector3 self) => Invert(self, Axis.X);
+        public static Vector3 InvertY(this Vector3 self) => Invert(self, Axis.Y);
+        public static Vector3 InvertZ(this Vector3 self) => Invert(self, Axis.Z);
+        public static Vector3 InvertXY(this Vector3 self) => Invert(self, Axis.XY);
+        public static Vector3 InvertXZ(this Vector3 self) => Invert(self, Axis.XZ);
+        public static Vector3 InvertYZ(this Vector3 self) => Invert(self, Axis.YZ);
+        public static Vector3 InvertAll(this Vector3 self) => -self;
+
+        private static Vector3 Invert(this Vector3 self, Axis axis) {
+            switch (axis) {
+                case Axis.X:
+                    return new Vector3(-self.x, self.y, self.z);
+                case Axis.Y:
+                    return new Vector3(self.x, -self.y, self.z);
+                case Axis.Z:
+                    return new Vector3(self.x, self.y, -self.z);
+                case Axis.XY:
+                    return new Vector3(-self.x, -self.y, self.z);
+                case Axis.XZ:
+                    return new Vector3(-self.x, self.y, -self.z);
+                case Axis.YZ:
+                    return new Vector3(self.x, -self.y, -self.z);
+                default:
+                    return self;
+            }
+        }
+
+        public static float GetRotationAngleX(this Vector3 self) => GetRotationAngle(self, Axis.X);
+        public static float GetRotationAngleY(this Vector3 self) => GetRotationAngle(self, Axis.Y);
+        public static float GetRotationAngleZ(this Vector3 self) => GetRotationAngle(self, Axis.Z);
+
+
+        private static float GetRotationAngle(this Vector3 self, Axis axis) {
+            //Returns the angle of the object's forward vector, as seen from the top
+            Vector3 direction = self;
+
+            float angleInDegrees = 0;
+
+            switch (axis) {
+                case Axis.X:
+                    angleInDegrees = Mathf.Atan2(direction.y, direction.z) * Mathf.Rad2Deg;
+                    break;
+                case Axis.Y:
+                    angleInDegrees = Mathf.Atan2(direction.z, direction.x) * Mathf.Rad2Deg;
+                    break;
+                case Axis.Z:
+                    angleInDegrees = (Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg);
+                    break;
+                case Axis.XY:
+                    break;
+                case Axis.XZ:
+                    break;
+                case Axis.YZ:
+                    break;
+                default:
+                    break;
+            }
+
+            return angleInDegrees;
+        }
+
+        public static Vector3 InverseTransformPoint(this Vector3 self, Vector3 sourcePosition, Quaternion sourceRotation, Vector3 sourceScale) {
+            Matrix4x4 matrix = Matrix4x4.TRS(sourcePosition, sourceRotation, sourceScale);
+            Matrix4x4 inverse = matrix.inverse;
+            return inverse.MultiplyPoint3x4(self);
+        }
+
+        public static Vector3 RotatePointAroundPivot(this Vector3 self, Vector3 pivot, Quaternion rotation) {
+            Vector3 output = self;
+            var dir = pivot - output;
+            dir = rotation * dir;
+            output = dir + pivot;
+            return output;
+        }
+
+        /// <summary>
+        /// Turn angle into vector direction
+        /// </summary>
+        /// <param name="angle">Horizontal degree value</param>
+        /// <returns></returns>
+        public static Vector3 HorizontalDirection(this float angle) {
+            angle *= Mathf.Deg2Rad;
+            return new Vector3(Mathf.Sin(angle), 0, Mathf.Cos(angle));
+        }
+
+
+        /// <summary>
+        /// Turn vector direction into degree
+        /// </summary>
+        /// <param name="vector">Horizontal Direction</param>
+        /// <returns></returns>
+        public static float HorizontalAngle(this Vector3 vector) {
+            var v = new Vector2(vector.z, vector.x);
+
+            if (v.sqrMagnitude > 0.01f)
+                v.Normalize();
+
+            var sign = (v.y < 0) ? -1.0f : 1.0f;
+            return Vector2.Angle(Vector2.right, v) * sign;
+        }
+    }
+}
